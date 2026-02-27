459a122ff9056f166e76613df739cb890a7be602
Mrs. Zando <mzando@columbus.k12.oh.us>
2026-02-27 10:33:45 -0500

    try again

--- assets/js/color-modes.js
+++ assets/js/color-modes.js
@@ -1,4 +1,4 @@
-/*!
+/*! does this work?
  * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
  * Copyright 2011-2025 The Bootstrap Authors
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
